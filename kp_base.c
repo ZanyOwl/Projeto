569ae92181bbf83cc67239dbd7d19990483fd483
zany <pedronevesepm@gmail.com>
2022-11-25 16:29:33 +0000

    Altera os comentarios e muda o scope do programa para ser focado em tempo

--- kp_base.c
+++ kp_base.c
@@ -12,10 +12,10 @@ int main()
     //weight é o peso maximo que a mala pode levar
     int weight = 0;
     //processes define o numero de processos que vão ser criados
-    int processes = 0;
-    //A matriz values vai ser do tamanho do numere de items e vai guarda as combinações de pares
-    //que vem no ficheiro de texto  
+    int processes = 1;
+    //O array values vai guardar os valores de cada item 
     int values[50];
+    //o array weights vai guardar os pesos dos items
     int weights[50];
 
     //Vai criar a memoria partilhada
@@ -27,17 +27,18 @@ int main()
     for(int i = 0; i < processes; i++){
         if (fork() == 0){
             int result = sol_kb(weight, weights, values, num_items);
-            //aqui vaí fazer a comparação do valor que o child recebeu com o valor da shared mem
+            //vai correr o algoritemo durante o tempo pedido no comando
+            //Vai fazer isto com um loop while
+            //A cada solução que encontrar, vai fazer o output "Child x has found solution y at time z"
         } else {
-            int result = sol_kb(weight, weights, values, num_items);
-            //aqui vaí fazer a comparação do valor que o child recebeu com o valor da shared mem
+            //vai matar os filhos
         }
     }
 
     return 0;
 }
 
-//Resolve o problema e devolve um valor para cada processo depois comparar com a solução do ficheiro
+//Resolve o problema e devolve um valor para cada processo depois compara com a solução do ficheiro
 //e puder colocar em memória partilhada
 int sol_kp(int weight, int weights[], int values[], int num_items){
     if (num_items == 0 || weight == 0){
