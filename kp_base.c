5167f22f7b44121e69a98b7824dc0b62b096c6ba
pedro <pedronevesepm@gmail.com>
2022-12-02 17:45:37 +0000

    o prgrama compila e corre, mas fica preso num só child. Penso que tem a haver com loop while mas não sei

--- kp_base.c
+++ kp_base.c
@@ -8,6 +8,12 @@
 #include <sys/wait.h>
 #include <string.h>
 
+#define MAX 100
+
+int sol_kp(int weight, int weights[], int values[], int num_items);
+int max(int x, int y);
+void readFile(char nameFile[],int *n,int *maxW,int v[],int w[]);
+
 void signal_handler(int signal){
     printf("Killed Child %d\n", signal);
 }
@@ -19,28 +25,22 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    char fileName[50];
-    strcpy(fileName, argv[2]);
-    
-    FILE *testFile = fopen(fileName, "r");
-    if(testFile == NULL){
-        printf("ERROR: File returned NULL!!!!");
-        return -1;
-    }
     //num_items Define o numero de items que podem ser postos dentro da mala
-    int num_items;
-    fscanf(testFile, "%d", &num_items);
+    int num_items, weight;
     //weight é o peso maximo que a mala pode levar
-    int weight = 0;
     //processes define o numero de processos que vão ser criados
     int processes = atoi(argv[3]);
     //time define o tempo que o programa vai correr
     int time = atoi(argv[4]);
     //O array values vai guardar os valores de cada item 
-    int values[50];
+    int values[MAX];
     //o array weights vai guardar os pesos dos items
-    int weights[50];
+    int weights[MAX];
     int pids[processes];
+
+    char nameFile[50];
+    strcpy(nameFile, argv[2]);
+    readFile(nameFile,&num_items,&weight,values,weights);
 
     //Vai criar a memoria partilhada
     int size = 64;
@@ -51,39 +51,38 @@ int main(int argc, char *argv[])
     sem_unlink("mymutex");
     sem_t *sem1 = sem_open("mymutex", O_CREAT, 0644, 1);
 
-    signal(SIGUSR1, signal_handler);
-
+    *((int *)shmem) = 1;
     for(int i = 0; i < processes; i++){
         if ((pids[i] = fork()) == 0){
             //vai correr o algoritemo durante o tempo pedido no comando
             //Vai fazer isto com um loop while
             //A cada solução que encontrar, vai fazer o output "Child x has found solution y at time z"
-            while(1){
-                int result = sol_kb(weight, weights, values, num_items);
+            do {
+                int result = sol_kp(weight, weights, values, num_items);
                 sem_wait(sem1);
-                if(result < shmem){
-                    shmem = result;
-                    printf("%s%d%s%d%s%d", "Child", getpid(),"has foand a solution: ", result, "at time", time);
+                if(result > *((int *)shmem)){
+                    *((int *)shmem) = result;
+                    printf("%s%d%s%d%s", "Child ", pids[i]," has found a solution: ", result, "\n");
                 }
                 sem_post(sem1);
                 result = 0;
-            }
+            } while(1);
         }
     }
     //O pai dorme até ao fim do tempo de execução
     sleep(time);
     //Agora vai matar os filhos
     for(int x = 0; x < processes; x++){
-        kill(pids[x], SIGUSR1);
+        kill(pids[x], SIGKILL);
     }
 
     //Agora vai aceder a memoria partilhada e vai escrever o valor final no ecrã e vai terminar o program
     sem_wait(sem1);
-    printf("%s%i", "Best Overall Solution Found: ", shmem);
+    printf("%s%d%s", "Best Overall Solution Found: ", *((int *)shmem), "\n");
     sem_post(sem1);
     sem_close(sem1);
 
-    return 0;
+    return (EXIT_SUCCESS);
 }
 
 //Resolve o problema e devolve um valor para cada processo depois compara com a solução do ficheiro
@@ -97,8 +96,23 @@ int sol_kp(int weight, int weights[], int values[], int num_items){
         return sol_kp(weight, weights, values, num_items - 1);
     }
     else {
-        return max(values[num_items - 1] + sol_kp(weight - weights[num_items - 1], weight, values, num_items - 1), sol_kp(weight, weights, values, num_items - 1));
+        return max(values[num_items - 1] + sol_kp(weight - weights[num_items - 1], weights, values, num_items - 1), sol_kp(weight, weights, values, num_items - 1));
     }
 }
 
 int max(int x, int y){return (x > y) ? x : y;}
+
+void readFile(char nameFile[],int *n,int *maxW,int v[],int w[]) {
+  FILE *f = fopen(nameFile,"r");
+
+  if (f == NULL) {
+    printf("Error reding file\n");
+    exit(-1);
+  }
+
+  fscanf(f,"%d",n);
+  fscanf(f,"%d",maxW);
+
+  for (int i=0; i < *n; i++)
+    fscanf(f,"%d %d",&v[i],&w[i]);
+}
